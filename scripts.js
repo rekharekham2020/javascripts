e4c1fd62f88249bc1646e9bd15cafde9f8889883
rekharekham2020 <rekharekham2020@gmail.com>
2024-05-15 15:35:48 +0530

    ss

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function validateage(){
+    var name = document.getElementById("name");
+    var age =document.getElementById("age");
+     if(age >=18){
+        alert("you are eligible to vote");
+     }else{
+        alert("you are still child");
+     }
+
+}
